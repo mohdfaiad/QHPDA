9e8ea915a6e0c02a0ac23f88bede239070e78661
unknown <thu@dp122.dptech.local>
2017-05-29 16:39:31 +0800

    no message

---
+++ PickModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QHMobile
+{
+    class PickModule
+    {
+        public int bookmark;
+        public string pwNo;
+        public string itemNo;
+        public string oriItem;
+        public string bin;
+        public string oriBin;
+        public decimal quatity;
+        public decimal excess;
+        public decimal shortage;
+        public bool proceed;
+        public decimal lineno;
+        public string location;
+        public Boolean emptyflag;
+        public Boolean conversionflag;
+        public Boolean conversionpostive; 
+        public string substituteItem;
+        public string substituteTank;
+        public string dummysubitem;
+        public Boolean isDummyBin;
+
+        
+    }
+}
